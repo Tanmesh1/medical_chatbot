ab68dde06d5bead3bff0ada1fb03f7e0d1ecbf6c
Tanmesh Joshi <tanmeshj2002@gamil.com>
2026-02-23 13:48:45 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "medical_chatbot",
+    version="0.1.0",
+    author = "Tanmesh Joshi",
+    author_email="joshi.tanmesh@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
